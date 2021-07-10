235ff1aa3baeb868ee300a97f2212194b82b355c
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-07-10 18:40:26 +0200

    Drop example that's no longer valid

--- spec/hcloud/client_spec.rb
+++ spec/hcloud/client_spec.rb
@@ -4,14 +4,10 @@ RSpec.describe HCloud::Client do
   subject(:client) { described_class.new(access_token: "access_token") }
 
   it "has an access_token" do
-    expect(client.access_token).to eq access_token
+    expect(client.access_token).to eq "access_token"
   end
 
   it "has a default endpoint" do
     expect(client.endpoint).to eq "https://api.hetzner.cloud/v1"
-  end
-
-  it "has a default connection" do
-    expect(described_class.connection).to be_nil
   end
 end
