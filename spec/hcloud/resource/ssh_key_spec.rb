a80f8b21343809cc145d40570e82b1f72bb2d635
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-07-16 22:30:28 +0200

    Add sorting/filtering to SSH key integration test

--- spec/hcloud/resource/ssh_key_spec.rb
+++ spec/hcloud/resource/ssh_key_spec.rb
@@ -1,48 +1,80 @@
 # frozen_string_literal: true
 
 RSpec.describe HCloud::SSHKey, integration: true, order: :defined do
-  let(:file) { File.read(HCloud.root.join("spec/fixtures/ssh_key.pub")) }
+  let(:file_one) { File.read(HCloud.root.join("spec/fixtures/one.pub")) }
+  let(:file_two) { File.read(HCloud.root.join("spec/fixtures/two.pub")) }
 
-  id = nil
+  id_one, id_two = nil
 
   it "creates a resource" do
-    # Create SSH key
-    ssh_key = described_class.new(name: "My SSH Key", public_key: file)
+    ssh_key = described_class.new(name: "First SSH Key", public_key: file_one)
 
     ssh_key.create
 
     expect(ssh_key).to be_created
     expect(ssh_key.id).not_to be_nil
 
-    id = ssh_key.id
+    id_one = ssh_key.id
+  end
+
+  it "creates another resource" do
+    ssh_key = described_class.new(name: "Second SSH Key", public_key: file_two)
+
+    ssh_key.create
+
+    expect(ssh_key).to be_created
+    expect(ssh_key.id).not_to be_nil
+
+    id_two = ssh_key.id
   end
 
   it "finds a resource" do
-    ssh_key = described_class.find(id)
+    ssh_key = described_class.find(id_one)
 
-    expect(ssh_key.name).to eq "My SSH Key"
-    expect(ssh_key.public_key).to eq file
+    expect(ssh_key.name).to eq "First SSH Key"
+    expect(ssh_key.public_key).to eq file_one
+  end
+
+  it "lists resources" do
+    ssh_keys = described_class.all
+
+    expect(ssh_keys.count).to eq 2
+    expect(ssh_keys.map(&:id)).to match_array [id_one, id_two]
+  end
+
+  it "sorts resources" do
+    ssh_keys = described_class.all.sort(name: :desc)
+
+    expect(ssh_keys.count).to eq 2
+    expect(ssh_keys.map(&:id)).to eq [id_two, id_one]
+  end
+
+  it "filters resources" do
+    ssh_keys = described_class.all.where(name: "First SSH Key")
+
+    expect(ssh_keys.count).to eq 1
+    expect(ssh_keys.first.id).to eq id_one
   end
 
   it "updates a resource" do
-    ssh_key = described_class.find(id)
+    ssh_key = described_class.find(id_one)
 
     ssh_key.name = "My Other SSH Key"
 
     ssh_key.update
 
-    ssh_key = described_class.find(id)
+    ssh_key = described_class.find(id_one)
 
     expect(ssh_key.name).to eq "My Other SSH Key"
   end
 
   it "deletes a resource" do
-    ssh_key = described_class.find(id)
+    ssh_key = described_class.find(id_one)
 
     ssh_key.delete
 
     expect(ssh_key).to be_deleted
 
-    expect { described_class.find(id) }.to raise_error HCloud::Errors::NotFound
+    expect { described_class.find(id_one) }.to raise_error HCloud::Errors::NotFound
   end
 end
