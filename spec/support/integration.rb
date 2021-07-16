3d58ffcb0392652f4e71a203b024d2b4928cd492
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-07-16 19:49:46 +0200

    Save and restore client before integration spec

--- spec/support/integration.rb
+++ spec/support/integration.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  config.before(integration: true) do
+  config.before(:context, integration: true) do
+    @client = HCloud::Client.connection
+
     # Set client
-    HCloud::Client.connection = HCloud::Client.new(access_token: ENV.fetch("HCLOUD_TOKEN"))
+    HCloud::Client.connection = HCloud::Client.new(
+      access_token: ENV.fetch("HCLOUD_TOKEN"),
+    )
 
     # Enable real HTTP connections
     WebMock.allow_net_connect!
   end
 
-  config.after(integration: true) do
+  config.after(:context, integration: true) do
     # Delete all created instances
     described_class.all.each(&:delete)
 
     # Unset client
-    HCloud::Client.connection = nil
+    HCloud::Client.connection = @client
 
     # Disable real HTTP connections
     WebMock.disable_net_connect!
