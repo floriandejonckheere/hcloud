b6f4e826011a77cfd844fee63131c0103c96a60f
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-07-18 15:11:03 +0200

    Make log level adjustable in console and specs

--- spec/support/integration.rb
+++ spec/support/integration.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
+  logger = Logger.new($stdout)
+  logger.level = ENV.fetch("LOG_LEVEL", "warn")
+
   config.before(:context, integration: true) do
     @client = HCloud::Client.connection
 
     # Set client
     HCloud::Client.connection = HCloud::Client.new(
       access_token: ENV.fetch("HCLOUD_TOKEN"),
+      logger: logger,
     )
 
     # Enable real HTTP connections
