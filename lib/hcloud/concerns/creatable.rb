b7b72574046c00268dd3efec4e21742c382c4f04
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-07-19 18:50:11 +0200

    Allow specifying nested attributes as creatable params

--- lib/hcloud/concerns/creatable.rb
+++ lib/hcloud/concerns/creatable.rb
@@ -9,7 +9,7 @@ module HCloud
 
       def create
         assign_attributes client
-          .post("/#{resource_name.pluralize}", attributes.slice(*creatable_attributes.map(&:to_s)))
+          .post("/#{resource_name.pluralize}", creatable_params)
           .fetch(resource_name.to_sym)
       end
 
@@ -19,6 +19,16 @@ module HCloud
 
       def creatable_attributes
         []
+      end
+
+      # Convert creatable_attributes into a key-value list
+      def creatable_params
+        nested_attributes, resource_attributes = creatable_attributes.partition { |a| a.respond_to? :each }
+
+        attributes
+          .slice(*resource_attributes.map(&:to_s))
+          .merge(nested_attributes.reduce(&:merge).map { |k, v| [k, send(k).send(v)] }.to_h)
+          .compact
       end
     end
   end
