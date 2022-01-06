40a4c6cfd29c566a6ee773ee9e65543584defd18
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-01-06 18:28:01 +0100

    Use integer for ID ffaking

--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -4,7 +4,7 @@ require "base64"
 
 FactoryBot.define do
   factory :ssh_key, class: "HCloud::SSHKey" do
-    id { FFaker::Guid.guid }
+    id { FFaker::Random.rand(111_111_111..999_999_999) }
     name { FFaker::Lorem.word }
     public_key { "ssh-rsa #{Base64.encode64(FFaker::Lorem.sentence)}" }
     fingerprint { FFaker::HEX.sample(32).each_cons(2).map(&:join).join(":") }
