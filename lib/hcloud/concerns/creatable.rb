71f1558e394305136168dd8ea27850fdf53e2583
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-01-09 16:36:47 +0100

    Allow passing in entities as creatable parameters

--- lib/hcloud/concerns/creatable.rb
+++ lib/hcloud/concerns/creatable.rb
@@ -22,16 +22,18 @@ module HCloud
       end
 
       # Convert creatable_attributes into a key-value list
-      # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity
+      # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
       def creatable_params
-        nested_attributes, resource_attributes = creatable_attributes.partition { |a| a.respond_to? :each }
+        # Split simple and nested attributes
+        nested_attributes, simple_attributes = creatable_attributes.partition { |a| a.respond_to? :each }
 
         attributes
-          .slice(*resource_attributes.map(&:to_s))
+          .slice(*simple_attributes.map(&:to_s))
+          .transform_values { |v| v&.send_wrap(:try, :to_h) || v&.send_wrap(:to_s) }
           .merge(nested_attributes.reduce(&:merge)&.map { |k, v| [k.to_s, Array(v).filter_map { |w| send(k)&.send_wrap(w) }.first] }.to_h)
           .compact
       end
-      # rubocop:enable Metrics/AbcSize,Metrics/CyclomaticComplexity
+      # rubocop:enable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
     end
 
     class_methods do
--- spec/hcloud/resource_spec.rb
+++ spec/hcloud/resource_spec.rb
@@ -7,6 +7,13 @@ end
 
 ActiveModel::Type.register(:child, HCloud::ResourceType.Type("Child"))
 
+class Sibling < HCloud::Entity
+  attribute :name
+  attribute :child, :child
+end
+
+ActiveModel::Type.register(:sibling, HCloud::ResourceType.Type("Sibling"))
+
 RSpec.describe HCloud::Resource do
   subject(:resource) { resource_class.new }
 
@@ -24,12 +31,14 @@ RSpec.describe HCloud::Resource do
       attribute :child, :child
       attribute :children, :child, array: true, default: []
 
+      attribute :sibling, :sibling
+
       attribute :labels, default: -> { {} }
 
       action :resize
 
       def creatable_attributes
-        [:name, :description, :labels, child: :name, children: [:id, :name]]
+        [:name, :description, :labels, :sibling, child: :name, children: [:id, :name]]
       end
 
       def updatable_attributes
@@ -48,14 +57,16 @@ RSpec.describe HCloud::Resource do
   end
 
   describe "#create" do
-    subject(:resource) { resource_class.new(id: nil, name: "my_resource", description: "my_description", created: nil, child: child1, children: [child0, child1]) }
+    subject(:resource) { resource_class.new(id: nil, name: "my_resource", description: "my_description", created: nil, sibling: sibling, child: child1, children: [child0, child1]) }
 
     let(:child0) { Child.new(id: 1) }
     let(:child1) { Child.new(name: "name1") }
 
+    let(:sibling) { Sibling.new(name: "sibling", child: child0) }
+
     it "creates the resource" do
       stub_request(:post, "https://api.hetzner.cloud/v1/resources")
-        .with(body: { name: "my_resource", description: "my_description", labels: {}, child: "name1", children: [1, nil] })
+        .with(body: { name: "my_resource", description: "my_description", labels: {}, sibling: { name: "sibling", child: { id: 1 } }, child: "name1", children: [1, nil] })
         .to_return(body: { resource: resource.attributes.merge(id: 1, created: 1.second.ago) }.to_json)
 
       resource.create
