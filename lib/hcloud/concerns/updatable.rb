24aa74f07bcc27e92881c3b5514a3bc10fbab677
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-01-11 20:47:28 +0100

    Add updatable_params

--- lib/hcloud/concerns/updatable.rb
+++ lib/hcloud/concerns/updatable.rb
@@ -7,12 +7,17 @@ module HCloud
     included do
       def update
         assign_attributes client
-          .put("/#{resource_name.pluralize}/#{id}", attributes.slice(*updatable_attributes.map(&:to_s)))
+          .put("/#{resource_name.pluralize}/#{id}", updatable_params)
           .fetch(resource_name.to_sym)
       end
 
       def updatable_attributes
         []
+      end
+
+      # Convert updatable_attributes into a key-value list
+      def updatable_params
+        attributes.slice(*updatable_attributes.map(&:to_s))
       end
     end
   end
