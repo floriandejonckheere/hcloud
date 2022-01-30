ea4394e90ecfe3e1cf2bc4601d1583fa3794dd45
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-01-30 23:00:26 +0100

    Compact and blank creatable attributes

--- lib/hcloud/concerns/creatable.rb
+++ lib/hcloud/concerns/creatable.rb
@@ -31,7 +31,7 @@ module HCloud
           .slice(*simple_attributes.map(&:to_s))
           .transform_values { |v| v&.send_wrap { |o| o.try(:to_h) || o } || v&.send_wrap(:to_s) }
           .merge(nested_attributes.reduce(&:merge)&.map { |k, v| [k.to_s, Array(v).filter_map { |w| send(k)&.send_wrap(w) }.first] }.to_h)
-          .compact
+          .compact_blank
       end
       # rubocop:enable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
     end
