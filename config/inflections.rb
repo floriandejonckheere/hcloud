6cf6979acbce0f39d49edd6aebe1f05354ff15d7
Florian Dejonckheere <florian@floriandejonckheere.be>
2022-02-02 22:35:39 +0100

    Sort inflections

--- config/inflections.rb
+++ config/inflections.rb
@@ -2,23 +2,23 @@
 
 HCloud.loader.inflector.inflect(
   "dns_pointer" => "DNSPointer",
-  "hcloud" => "HCloud",
-  "ssh_key" => "SSHKey",
   "floating_ip" => "FloatingIP",
   "floating_ip_price" => "FloatingIPPrice",
-  "floating_ips" => "FloatingIPs",
   "floating_ip_prices" => "FloatingIPPrices",
+  "floating_ips" => "FloatingIPs",
+  "hcloud" => "HCloud",
   "http" => "HTTP",
+  "ip_not_available" => "IPNotAvailable",
   "ipv4" => "IPv4",
   "ipv6" => "IPv6",
-  "ip_not_available" => "IPNotAvailable",
   "iso" => "ISO",
   "iso_type" => "ISOType",
+  "ssh_key" => "SSHKey",
 )
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym "DNS"
-  inflect.acronym "JSON"
   inflect.acronym "IP"
   inflect.acronym "ISO"
+  inflect.acronym "JSON"
 end
